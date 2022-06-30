7aba2411ab4837906eddb58b5dc8bd5ca5261224
YangDaEun <das03041@gmail.com>
2022-06-30 17:17:13 +0900

    feat : 내용 수정

---
+++ jsp/src/test/java/utils/ExcelVO.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class ExcelVO {
+	public String statesCode; // 구코드 
+	public String sportsCode; // 종목코드
+	public double Latitude; // 위도
+	public double Longitude; // 경도
+	
+}
+		
